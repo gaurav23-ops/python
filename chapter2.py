d7d5759661bc8e7b3f1c1a03645716cd06a635b8
gaurav23-ops <gauravkondane23@gmail.com>
2025-07-14 11:43:46 +0530

    operator

---
+++ chapter2.py
@@ -0,0 +1,25 @@
+#arithematic opretors (+ - * )
+a = 1
+b = 2
+c = a+b
+print(c)
+
+# Assignment operators: =, +=, -= etc
+b = 3
+b += 6
+print(b)
+
+# Comparison operators: ==, >, >=, <, != etc.
+a =5==5
+print(a)
+d = 5!=5
+print(d)
+
+# . Logical operators: and, or, not
+# In 'or' only if false or false is false
+# In 'and' only if true and true is true
+# In Not(true)= false, Not(false) = true
+
+print ("false or False is", False or False)
+print ("True and true is", True and True)
+print(not(True))
