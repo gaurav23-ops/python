f5f28ca9691939ed7b8c3d8455cc870407f33198
gaurav23-ops <gauravkondane23@gmail.com>
2025-07-14 10:03:40 +0530

    module add

---
+++ chapter1.ps.py
@@ -0,0 +1,6 @@
+#que 3 to install the module in this i innstall module which work on text to speech 
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("what are you doing priyanka kulkarni ")
+engine.runAndWait()
