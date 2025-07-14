cc206c70f60634a9195287d7a13340f387d66615
gaurav23-ops <gauravkondane23@gmail.com>
2025-07-14 16:38:17 +0530

    practice2

---
+++ chapter2.ps.py
@@ -0,0 +1,49 @@
+#Q1 Write a python program to add two numbers.
+a = 2
+b = 3
+print("number a is:",a)
+print("number b is:",b)
+print("sum is ", a+b)
+
+#Q2 Write a python program to find remainder when a number is divided by z.
+c = 34
+z = 5
+print("remainder if devided ", c%z)
+
+# Q3 Check the type of variable assigned using input () function.
+
+a = input("enter the value:")
+print (" the value is:",a)
+# b = int(a)
+t = type(a)
+print(t)
+
+# refer Q3 if we have to change the type in input vaule only how we change so let go i will tell
+d= input("enter the value:")
+print (" the value is:",d)
+n= int(d)
+t = type(n)
+print(t)
+
+#Q4  Use comparison operator to find out whether ‘a’ given variable a is greater than
+# ‘b’ or not. Take a = 34 and b = 80
+
+a = 34 
+b = 80 
+# c = a>b
+print("a is greater than b:",a>b)
+
+#Q5 Write a python program to find an average of two numbers entered by the user
+a = int(input("Enter the value1:"))
+b = int(input("Enter the value2:"))
+print("the number 1 is :",a)
+print("the number 2 is :",b)
+print("the avrage of that number",(a+b)/2)
+
+# . Write a python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter the numbre"))
+print("the number is :",a)
+print("the answer is:", a*a)
+
+
