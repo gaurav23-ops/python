20adc1f7e031d6f791f93d430ee524930bd12766
gaurav23-ops <gauravkondane23@gmail.com>
2025-07-16 00:06:40 +0530

    dict and set

---
+++ chapter5.py
@@ -0,0 +1,27 @@
+#dictionary  d={} this empty set
+
+'''• a.items(): Returns a list of (key,value)tuples.
+• a.keys(): Returns a list containing dictionary's keys.
+• a.update({"friends":}): Updates the dictionary with supplied key-value pairs.
+• a.get("name"): Returns the value of the specified keys (and value is returned
+eg."harry" is returned here).
+
+'''
+marks = {
+    "gaurav":100,
+    "priyanka":33,
+    "arrti":99
+}
+# marks.items()
+# print(marks.keys())
+# marks.update({"gaurav":99 , "pawan":56})
+# print(marks.get("gaurav1"))
+# marks.pop("gaurav")
+marks.popitem()
+print(marks)
+
+#set 
+s =  {1,2,3}#this is set 
+a=set()# is empty set
+
+
