6f6588cc3273ef84d396c6d169f7073bbc129151
gaurav23-ops <gauravkondane23@gmail.com>
2025-07-15 21:12:02 +0530

    list and tuple

---
+++ chapter4.py
@@ -0,0 +1,13 @@
+#list its used square bracket
+friend = ["Apple", "grapes", 5 , 2.0 , "gaurav", "sham"]
+print(friend[0])
+friend[0] = "mango"
+print(friend[0])
+
+l1= [1,23,23,5,1,45,3,1,25,12,]
+#l1.sort()
+# p=l1.pop(3)
+# print(p)
+# print(l1)
+l1.remove(23)
+print(l1)
