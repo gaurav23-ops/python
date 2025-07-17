ff1a88e6968b4c9d620c4e78553c1d86b9ac677a
gaurav23-ops <gauravkondane23@gmail.com>
2025-07-17 20:21:30 +0530

    add code

---
+++ chapter7.ps_loop.py
@@ -0,0 +1,35 @@
+# n = int(input("enter number: "))
+# i = 1
+# for i in  range (1, 11):
+#     print(n*i)
+#     i = i+1
+
+
+
+#Q2 Write a program to greet all the person names stored in a list ‘l’ and which starts
+# with S.
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+# for  i in l:
+#     if i.startswith("S"):
+#         print("Hello",i)
+
+#Q3 Attempt problem 1 using while loop.
+# n = int(input("enter number: "))
+# i = 1
+# while (i < 11):
+# #     print(n*i)
+#     i = i+1
+
+
+#Q4 Write a program to find whether a given number is prime or not
+
+n = int(input("enter the number"))
+for n in  range   :
+    if (n % 2==0):
+     print("it prime number")
+    else:
+     print("not prime")
+
+
---
+++ escape_sequence.py
@@ -0,0 +1,3 @@
+print("Hey i am\ngood boy ")# here i used \n for new line
+print("Hey i am \"good boy\"\nbut viewer is also \"good boy\"") # here i used \"" for word to get bold 
+print("Hey", 6 ,7 , sep="$" , end="23") # here i used sep and end to get speration and end with given
